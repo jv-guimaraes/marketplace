1fb1d627c73476313240eea6d9f54ca7b29bbc68
jv-guimaraes <joao.guimaraes@aluno.uepb.edu.br>
2023-05-28 18:19:44 -0300

    Testar transformar uma lista de objetos em json

--- src/main/java/marketplace/Main.java
+++ src/main/java/marketplace/Main.java
@@ -1,13 +1,21 @@
 package marketplace;
 
-import com.google.gson.Gson;
+import java.util.ArrayList;
+
+import org.json.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Gson gson = new Gson();
-		Teste teste = new Teste(69, "Testando");
-		System.out.println(gson.toJson(teste));
+		Teste teste1 = new Teste(25, "Testando uma string");
+		Teste teste2 = new Teste(40, "Testando string");
+		Teste teste3 = new Teste(123, "Blah blah blah");
+		ArrayList<Teste> lista = new ArrayList<Teste>();
+		lista.add(teste1);
+		lista.add(teste2);
+		lista.add(teste3);
+		JSONArray jo = new JSONArray(lista);
+		System.out.println(jo);
 	}
 
 }
